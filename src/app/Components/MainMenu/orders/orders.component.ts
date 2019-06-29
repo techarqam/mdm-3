69da2b51b5a6be321befd24bba61a392c044762f
Mohammed Arqam <techarqam@gmail.com>
2019-06-30 02:00:06 +0530

    orders created

--- src/app/Components/MainMenu/orders/orders.component.ts
+++ src/app/Components/MainMenu/orders/orders.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { OrdersService } from '../../../Services/Orders/orders.service';
+import { Observable } from 'rxjs';
+import { AlertController } from '@ionic/angular';
+import { CommonService } from '../../../Services/Common/common.service';
 
 @Component({
   selector: 'app-orders',
@@ -7,9 +11,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrdersComponent implements OnInit {
   name: string = 'Orders';
+  orders: Observable<any>;
+  showLoader: boolean = true;
 
-  constructor() { }
+  constructor(
+    public orderService: OrdersService,
+    public alertCtrl: AlertController,
+    public commonService: CommonService,
+  ) {
+    this.getOrders();
+  }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
+
+  filterOrder(catId) {
+    if (catId) {
+      this.getOrderBySatus(catId);
+    } else {
+      this.getOrders();
+    }
+  }
+
+  getOrderBySatus(catId) {
+    this.showLoader = true;
+    this.orders = this.orderService.getStatusAOrders(catId);
+    this.orders.subscribe(() => { this.showLoader = false });
+  }
+
+  getOrders() {
+    this.showLoader = true;
+    this.orders = this.orderService.getAllAOrders();
+    this.orders.subscribe(() => { this.showLoader = false });
+  }
+
+  async deliver(id) {
+    const alert = await this.alertCtrl.create({
+      header: 'Deliver the order ?',
+      subHeader: 'Is the order completed ? ',
+      buttons: [
+        {
+          text: 'Not Delivered',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+          }
+        }, {
+          text: 'Delivered',
+          handler: () => {
+            this.orderService.deliverOrder(id).then(() => {
+              this.commonService.presentToast("Order Delivered");
+            })
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+
+  }
 }
--- src/app/Services/Orders/orders.service.ts
+++ src/app/Services/Orders/orders.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
-
+import { AngularFirestore } from '@angular/fire/firestore';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import * as firebase from 'firebase';
 @Injectable({
   providedIn: 'root'
 })
 export class OrdersService {
 
-  constructor() { }
+  constructor(
+    public db: AngularFirestore,
+  ) { }
+
+  orders = new FormGroup({
+    status: new FormControl("")
+  });
+
+  getAllAOrders() {
+    return this.db.collection("Orders", ref => ref.where("storeId", "==", firebase.auth().currentUser.uid)).snapshotChanges()
+  }
+  getStatusAOrders(status) {
+    return this.db.collection("Orders", ref => ref.where("storeId", "==", firebase.auth().currentUser.uid).where("status", "==", status)).snapshotChanges()
+  }
+  deliverOrder(id) {
+    return this.db.collection("Orders").doc(id)
+      .set({ status: "Completed" }, { merge: true });
+  }
 }
